2f829130667107c478197ded436244bf4dd1739f
mohitagarwal <mohit.tibrewal@flipkart.com>
2015-06-05 15:35:59 +0530

    Initial Commit with Floating view

---
+++ HackDay6-Android/app/src/main/java/com/flipkart/hackday6/Slide/SpanTwo.java
@@ -0,0 +1,26 @@
+package com.flipkart.hackday6.Slide;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.flipkart.hackday6.R;
+
+public class SpanTwo extends Fragment {
+
+    private FragmentActivity fa;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        fa = super.getActivity();
+        View two = inflater.inflate(R.layout.span_two, container, false);
+
+
+        return two;
+    }
+
+}
